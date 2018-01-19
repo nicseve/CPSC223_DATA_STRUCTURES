81d35abd8bfeaa5ad5522451b9a97224da00310c
Nicholas Seve <nicseve1@gmail.com>
2018-01-19 10:38:20 -0500

    fixed order of ending symbolic end

--- hw1/main.c
+++ hw1/main.c
@@ -4,38 +4,34 @@
 #include <stdlib.h>
 #include <assert.h>
 
-
-
 char* letswap(char *s,int len){  //word modifer
 
 	char *s2;
 	char *s7;
 	char *s8 = (char *)malloc(sizeof(char) * 256);
-	char s3[len+3];
-	char s4[len+3];
+	char s3[len];
+	char s4[len];
 
 	char end1[] = "an";
 	char end2[] = "o";
 
 	int k = 0;
-	int i = 0;
+	int i = len-1;
 	int j = len-1;
 
    //CHECK FOR SYMBOLS AT BEG AND END OF WORD
 	while( !(isalpha(s[k]))){
 		s3[k] = s[k];
 		k++;
-	}//move forward until first alpha char seen
+	}
 
     s3[k] = '\0'; //BEG OF WORD
 	s8 = strcat(s8,s3);
 
-
 	while(  !(isalpha(s[j])) ){
-		s4[i++] = s[j--];
+		s4[i--] = s[j--];
 	}
-	s4[i] = '\0'; //END OF WORD
-	//printf("%s\n",s4);
+	s4[len] = '\0';   //END OF WORD
 
 	char temp = s[k]; //store first alpha char of word
 
@@ -43,16 +39,13 @@ char* letswap(char *s,int len){  //word modifer
 		if(isupper(temp)){   //if first char upper chase
           s[k+1] = toupper(s[k+1]); //k+1 to get next alphachar?? was 1 before
 		  temp = tolower(temp);
-		  //s[len] = temp;
 		  s[++j] = temp;
 		}
 
 		else{
-			//s[len] = temp;
 			s[++j] = temp;
 		}
 	}
-
     //word is 1 or less chars so return with "an" appended
 	else {
 		return s2 = strcat(s,end1);
@@ -61,31 +54,21 @@ char* letswap(char *s,int len){  //word modifer
 	//NULL TERMINATE TRIMMED ORIGINAL WORD
 	s[j+1] = '\0';
 
-
-   //CONCAT APPROPRIATE ENDING
+   //CONCAT APPROPRIATE ENDINGS & BEGINNINGS
 	if(j-k <= 3){  //if length of word less than 3 concat end1
 		s2 = strcat(s,end1); //S2 HOLDS WORD WITH CORRECT ENDING
-		//printf("%s\n",&s2[1]);
-		//ADD SYMBOLIC ENDING  STORED IN S4, S2 TRUNCATED AT K
-		s7 = strcat(&s2[k],s4);
-
-		//add symb beginning to truncated
-		s8 = strcat(s8,&s7[1]);
+		s7 = strcat(&s2[k],&s4[i+1]); //ADD SYMBOLIC ENDING  STORED IN S4, S2 TRUNCATED AT K
+		s8 = strcat(s8,&s7[1]); //add symb beginning
 		return s8;
 	}
 
 	else{
 		s2 = strcat(s,end2);
-		//printf("%s\n",&s2[1]);
-		s7 = strcat(&s2[k],s4);
+		s7 = strcat(&s2[k],&s4[i+1]);
 		s8 = strcat(s8,&s7[1]);
 		return s8;
 	}
-
-
-	return &s2[k];
-	}
-
+}
 
 //read words of input string;
 void wordread(char *s){
@@ -105,17 +88,14 @@ void wordread(char *s){
 
 			p = 0; //RESET P INDEX
 			word[0] = '\0'; //RESET WORD ARRAY
-			}
+		}
 
 		else{
            word[p++] = s[i]; //ADD LETTER FROM STRING TO WORD
-			}
-
-		i++;
 		}
+		i++;
 	}
-
-
+}
 
 int main(int argc, char **argv)
 {
@@ -132,15 +112,14 @@ int main(int argc, char **argv)
 		if(length >= size-1){
 			size *= 2;
 			line = realloc(line,size);
-			}
-
+		}
 		if(isspace(c)){
 			size += 2;
 			line = realloc(line,size);
-			}
+		}
 
 		line[length++] = c;
-		}
+	}
 
 	line[length] = '\0';
 	wordread(line);
